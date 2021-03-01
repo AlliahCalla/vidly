c11b632416ecaac3c8e04a6a436190c62091f52a
Alliah <alliahczarielle17@gmail.com>
2021-03-01 13:44:10 +1300

    Added toastify dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Redirect, Switch } from "react-router-dom";
+import {ToastContainer} from 'react-toastify';
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
@@ -7,12 +8,13 @@ import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
-
+import 'react-toastify/dist/ReactToastify.css';
 import "./App.css";
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <NavBar />
       <main className="container">
         <Switch>
