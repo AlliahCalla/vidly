8d325bb73e06053531e88ba4b70645e3794fe54c
Alliah <alliahczarielle17@gmail.com>
2021-03-02 12:27:19 +1300

    Deleted fake services

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,19 +1,31 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+import { ErrorBoundary } from "@sentry/react";
 
 const apIEndpoint = apiUrl + "/movies";
+
+function movieUrl(id) {
+  return `${apIEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apIEndpoint);
 }
 
 export function getMovie(id) {
-  return http.get(apIEndpoint + "/" + id);
+  return http.get(movieUrl(id));
 }
 
 export function deleteMovie(id) {
-  return http.delete(apIEndpoint + "/" + id);
+  return http.delete(movieUrl(id));
 }
 
 export function saveMovie(movie) {
-  console.log("save movie");
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+
+  return http.post(apIEndpoint, movie);
 }
