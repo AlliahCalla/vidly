2387db844fccd6618788b928ee595b65e93e6c78
Alliah <alliahczarielle17@gmail.com>
2021-03-12 15:03:10 +1300

    Protected new movie route

--- src/App.js
+++ src/App.js
@@ -19,21 +19,31 @@ class App extends Component {
 
   componentDidMount() {
     const user = auth.getCurrentUser();
-    this.setState({user});
+    this.setState({ user });
   }
 
   render() {
+    const { user } = this.state;
     return (
       <>
         <ToastContainer />
-        <NavBar user={this.state.user} />
+        <NavBar user={user} />
         <main className="container">
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/logout" component={Logout} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies}></Route>
+            <Route
+              path="/movies/:id"
+              render={(props) => {
+                if (!user) return <Redirect to="/login" />;
+                return <MovieForm {...props} />;
+              }}
+            />
+            <Route
+              path="/movies"
+              render={(props) => <Movies {...props} user={this.state.user} />}
+            ></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
             <Route path="/not-found" component={NotFound}></Route>
